343c0fe4526a3b754aae0f6d6071928648d01fc0
Agustin-Sabando <agustinsabando18@gmail.com>
2022-08-08 11:50:33 -0300

    Aplicando al e-commerce .ejs

--- site/app.js
+++ site/app.js
@@ -3,14 +3,26 @@ const path = require('path')
 const app = express()
 const port = 3000
 
+/* Requerir las rutas */
+let indexRouter = require('./routes/index')
+
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'home.html')))
-app.get('/crearCuenta', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'register.html')))
-app.get('/ingresar', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'login.html')))
-app.get('/detalleDeProducto', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'productDetail.html')))
-app.get('/procesoDeCompra1', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'cart1.html')))
-app.get('/procesoDeCompra2', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'cart2.html')))
-app.get('/categorias', (req, res) => res.sendFile(path.resolve(__dirname, 'views', 'categories.html')))
+/* View engine setup */
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine',  'ejs')
+
+/* Middlewares */
+app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'public')))
+
+/* Rutas */
+app.use('/', indexRouter)
+app.use('/categories', indexRouter)
+app.use('/cart1', indexRouter)
+app.use('/cart2', indexRouter)
+app.use('/login', indexRouter)
+app.use('/productDetail', indexRouter)
+app.use('/register', indexRouter)
 
 app.listen(port, () => console.log(`Servidor abierto en http://localhost:${port}`))
---
+++ site/controllers/indexController.js
@@ -0,0 +1,24 @@
+
+module.exports = {
+    home: (req,res) => {
+        return res.render('home')
+    },
+    categories: (req,res) => {
+        return res.render('categories')
+    },
+    cart1: (req,res) => {
+        return res.render('cart1')
+    },
+    cart2: (req,res) => {
+        return res.render('cart2')
+    },
+    login: (req,res) => {
+        return res.render('login')
+    },
+    productDetail: (req,res) => {
+        return res.render('productDetail')
+    },
+    register: (req,res) => {
+        return res.render('register')
+    }
+}
