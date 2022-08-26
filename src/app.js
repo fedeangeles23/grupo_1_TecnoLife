b7e0b9309ccf2c9f3a42f38fa6cd0145c4d44d11
FedeAngeles23 <fedeangeles23@gmail.com>
2022-08-26 13:24:32 -0300

    New Slider witch events

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000
+
+/* Requerir las rutas */
+let indexRouter = require('./routes/index')
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+/* View engine setup */
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine',  'ejs')
+
+/* Middlewares */
+app.use(express.json())
+app.use(express.static(path.resolve(__dirname, 'public')))
+
+/* Rutas */
+app.use('/', indexRouter)
+app.use('/categories', indexRouter)
+app.use('/cart1', indexRouter)
+app.use('/cart2', indexRouter)
+app.use('/login', indexRouter)
+app.use('/productDetail', indexRouter)
+app.use('/register', indexRouter)
+
+app.listen(port, () => console.log(`Servidor abierto en http://localhost:${port}`))
