621010f3eed4453c03ef69c2e7f87e4c27aedb6b
Agustin-Sabando <agustinsabando18@gmail.com>
2022-08-27 21:11:36 -0300

    Implementación de CRUD (nuevo, edita, eliminar)

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
-let {home,categories,cart1,cart2,login,productDetail,register} = require('../controllers/indexController')
+let {home,categories} = require('../controllers/indexController')
 
 router.get('/', home)
 router.get('/categories', categories)
-router.get('/cart1', cart1)
-router.get('/cart2', cart2)
-router.get('/login', login)
-router.get('/productDetail', productDetail)
-router.get('/register', register)
 
 module.exports = router
