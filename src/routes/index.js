459bef1d0383615bd829ad0c6e83565932a4b6ec
Agustin-Sabando <agustinsabando18@gmail.com>
2022-11-08 11:28:32 -0300

    cambios en el package.json

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-let {home,categories} = require('../controllers/indexController')
-
-router.get('/', home)
-router.get('/categories', categories)
-
-module.exports = router
