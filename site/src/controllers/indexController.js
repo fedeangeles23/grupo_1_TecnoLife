b28ff8afc95ff6579b854225f94826d991ed86ad
Agustin-Sabando <agustinsabando18@gmail.com>
2022-12-22 10:04:02 -0300

    cambios db

--- site/src/controllers/indexController.js
+++ site/src/controllers/indexController.js
@@ -4,48 +4,39 @@ const { Op } = require("sequelize");
 module.exports = {
     home: (req, res) => {
     //    let aside = db.Asides.findAll()
-        /* let marcas = db.Marcas.findAll() */
 
-        /* let ofertasNotebooks = db.Categories.findAll() */
-        /* let ofertasNotebooks = db.Productos.findAll({
+        let marcas = db.Marcas.findAll()
+        
+        let ofertasNotebooks = db.Productos.findAll({
             where: {
                 categorias_id : 1
             },
-            include[
-                {all: true}
+            include: [
+                {all : true} 
             ]
-            Promise.all([marcas, ofertasNotebooks, smarts]).then(([marcar, ofertasNotebooks, smarts]) => {
-                return res.send(marcas)
-                return res.send(ofertasNotebooks)
-                return res.render("home", {
-                    marcas,
-                    ofertasNotebooks
-                })
-            }
-            let smarts = db.Productos.findAll({
+        })
+
+        let smarts =  db.Productos.findAll({
             where: {
                 categorias_id : 4
             },
-            include[
-                {all: true}
+            include: [
+                {all : true} 
             ]
-        }
-        }) */
-
-
-
-         db.Categorias.findAll({
-            include: [{all: true}]
         })
-    
-            .then(productos => {
-                /* res.send(productos) */
-/*                 return res.render('home',
-                    {
-                        productos
-                }) */
+         
+        Promise.all([marcas, ofertasNotebooks, smarts])
+        .then(([marcas, ofertasNotebooks, smarts]) => {
+            //return res.send(ofertasNotebooks)
+            return res.render("home", {
+                marcas,
+                ofertasNotebooks,
+                smarts
             })
-            .catch(error => res.send(error))
+
+        })
+        .catch(error => res.send(error))
+      
     },
     home2 :(req, res)=> {
         res.render('home')
--- site/src/routes/admin.js
+++ site/src/routes/admin.js
@@ -1,12 +1,12 @@
 const {list,create,edit,store,update,destroy,history,restore,crash} = require('../controllers/adminController')
 const express = require('express');
 const router = express.Router();
-const upload = require('../middlewares/multerProducts')
+const upload = require('../middlewares/multerProductos')
 const productValidator = require('../validations/productsValidation')
 
 /* GET home page. */
 router.get('/list', list);
-router.get('/history', history);
+/* router.get('/history', history); */
 
 /* Creando un producto */
 router.get('/create', create);
@@ -19,6 +19,6 @@ router.put('/edit/:id',upload.array('imagen'),productValidator, update);
 
 /* Eliminando un producto */
 router.delete('/destroy/:id', destroy);
-router.delete('/destroy/:id', restore);
+/* router.delete('/destroy/:id', restore); */
 
 module.exports = router;
