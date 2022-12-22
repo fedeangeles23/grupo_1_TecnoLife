200c185ffd7d76f36eaae2af0983e8566cf9e9d2
Agustin-Sabando <agustinsabando18@gmail.com>
2022-12-21 21:11:54 -0300

    a

--- site/src/controllers/indexController.js
+++ site/src/controllers/indexController.js
@@ -4,16 +4,46 @@ const { Op } = require("sequelize");
 module.exports = {
     home: (req, res) => {
     //    let aside = db.Asides.findAll()
-         db.Productos.findAll({
+        /* let marcas = db.Marcas.findAll() */
+
+        /* let ofertasNotebooks = db.Categories.findAll() */
+        /* let ofertasNotebooks = db.Productos.findAll({
+            where: {
+                categorias_id : 1
+            },
+            include[
+                {all: true}
+            ]
+            Promise.all([marcas, ofertasNotebooks, smarts]).then(([marcar, ofertasNotebooks, smarts]) => {
+                return res.send(marcas)
+                return res.send(ofertasNotebooks)
+                return res.render("home", {
+                    marcas,
+                    ofertasNotebooks
+                })
+            }
+            let smarts = db.Productos.findAll({
+            where: {
+                categorias_id : 4
+            },
+            include[
+                {all: true}
+            ]
+        }
+        }) */
+
+
+
+         db.Categorias.findAll({
             include: [{all: true}]
         })
     
             .then(productos => {
                 /* res.send(productos) */
-                return res.render('home',
+/*                 return res.render('home',
                     {
                         productos
-                })
+                }) */
             })
             .catch(error => res.send(error))
     },
