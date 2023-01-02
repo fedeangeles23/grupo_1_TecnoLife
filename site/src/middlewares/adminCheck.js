426f1fc18f013649980a60c12d9ecba06ee842bd
Fede Angeles <fedeangeles23@gmail.com>
2023-01-02 06:48:29 -0300

    ..

--- site/src/middlewares/adminCheck.js
+++
@@ -1,8 +0,0 @@
-module.exports = (req,res,next) => {
-    if (req.session.userLogin) {
-        if (req.session.userLogin.rol === 'admin') {
-            return next()
-        }
-    }
-    res.redirect('/')
-}
